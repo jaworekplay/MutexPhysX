1a70ae1d2a66de9d02ec4c5554af6001cc0999f8
Lukasz <jaworekplay@gmail.com>
2013-02-01 19:11:29 +0000

    Cleaning part 2

--- MutexPhysX.h
+++ MutexPhysX.h
@@ -48,6 +48,15 @@ private:
 	irr::core::dimension2d<irr::u32> size;
 	irr::video::E_DRIVER_TYPE driverType;
 	irr::IrrlichtDevice* device;
+	irr::scene::IMesh* mesh;
+	irr::scene::ISceneNode* m_rootNode;
+	irr::core::vector3df m_pos;
+	irr::core::vector3df m_rot;
+	irr::core::vector3df m_scale;
+	std::vector<int> m_vNPC;
+	std::vector<int> m_vPxNPC;
+	std::vector<int> m_vObj;
+	std::vector<int> m_vPxObj;
 	CMutex* mute;
 public:
 	irr::scene::ISceneManager* smgr;
@@ -62,7 +71,9 @@ public:
 		device = createDevice(driverType,size,16,false,false,false,rec);
 		smgr = device->getSceneManager();
 		driver = device->getVideoDriver();
-		
+		m_rootNode = smgr->getRootSceneNode();
+		m_pos = m_rot = irr::core::vector3df(0,0,0);
+		m_scale = irr::core::vector3df(1,1,1);
 		/*if( rec->isKeyPressed( irr::KEY_KEY_A ) )
 			mute->moveLeft();
 		if( rec->isKeyPressed( irr::KEY_KEY_D ) )
@@ -82,7 +93,17 @@ public:
 public:
 	virtual bool addNPC(){return true;}
 	virtual bool addPhysXNPC(){return true;}
-	virtual bool addObject(){return true;}
+	virtual bool addObject(irr::io::path& fileName, char* ID, irr::core::vector3df& Position)
+	{
+		int id = (int)ID;
+		mesh = smgr->getMesh(fileName);
+		if( Position.equals( m_pos ) )
+			smgr->addMeshSceneNode(mesh,m_rootNode,id,m_pos,m_rot,m_scale);
+		else
+			smgr->addMeshSceneNode(mesh,m_rootNode,id,Position,m_rot,m_scale);
+		m_vObj.push_back(id);
+		return true;
+	}
 	virtual bool addPhysXObject(){return true;}
 	virtual bool addPlayerCharacter(){return true;}
 	virtual bool render()
